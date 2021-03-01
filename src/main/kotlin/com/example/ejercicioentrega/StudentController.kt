0e40c64d662833f1e6dceb1249769d915ef59509
Javier <javiermiguel9903@gmail.com>
2021-03-01 19:35:53 +0100

    aa

--- src/main/kotlin/com/example/ejercicioentrega/StudentController.kt
+++ src/main/kotlin/com/example/ejercicioentrega/StudentController.kt
@@ -1,10 +1,44 @@
 package com.example.ejercicioentrega
 
 import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
+import java.security.MessageDigest
+import javax.crypto.BadPaddingException
+
+
+private fun cifrar(textoEnString : String, llaveEnString : String) : String {
+    println("Voy a cifrar $textoEnString")
+    val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    cipher.init(Cipher.ENCRYPT_MODE, getKey(llaveEnString))
+    val textCifrado = Base64.getEncoder().encodeToString(cipher.doFinal(textoEnString.toByteArray(Charsets.UTF_8)))
+    println("He obtenido $textCifrado")
+    return textCifrado
+}
+@Throws(BadPaddingException::class)
+private fun descifrar(textoCifrrado : String, llaveEnString : String) : String {
+    println("Voy a descifrar $textoCifrrado")
+    val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    cipher.init(Cipher.DECRYPT_MODE, getKey(llaveEnString));
+    val textDescifrado = String(cipher.doFinal(Base64.getDecoder().decode(textoCifrrado)))
+    println("He obtenido $textDescifrado")
+    return textDescifrado
+}
+
+
+private fun getKey(llaveEnString : String): SecretKeySpec {
+    var llaveUtf8 = llaveEnString.toByteArray(Charsets.UTF_8)
+    val sha = MessageDigest.getInstance("SHA-1")
+    llaveUtf8 = sha.digest(llaveUtf8)
+    llaveUtf8 = llaveUtf8.copyOf(16)
+    return SecretKeySpec(llaveUtf8, "AES")
+}
 
 @RestController
 class StudentController(private val trackRepository: TrackRepository) {
+
 
     //curl -v localhost:8081/getAllTracks
     @GetMapping("/getAllTracks")
@@ -27,7 +61,10 @@ class StudentController(private val trackRepository: TrackRepository) {
     //curl -v localhost:8081/givemetrack/12
     @GetMapping("/givemetrack/{id}")
     fun getTracks(@PathVariable id:Long) : Track{
-        return trackRepository.findById(id).get()
+        val cifrar= id.toString();
+        val key="FUAA"
+        val descifrada=descifrar(cifrar(cifrar,key),key)
+        return trackRepository.findById(descifrada.toLong()).get()
     }
 
     //curl -v localhost:8081/deletetrack/12
